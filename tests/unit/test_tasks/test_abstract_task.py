de61f77f6908becac6f074a79a138691e7b46402
Michal Kononenko <michalkononenko@gmail.com>
2017-03-02 11:47:35 -0500

    Added documentation for the abstract class unit tests.

--- tests/unit/test_tasks/test_abstract_task.py
+++ tests/unit/test_tasks/test_abstract_task.py
@@ -5,20 +5,41 @@ from mr_freeze.tasks.abstract_task import AbstractTask
 
 
 class ConcreteTask(AbstractTask):
+    """
+    Describes a task that only returns 1
+    """
     def task(self) -> int:
+        """
+
+        :return: The number ``1``
+        """
         return 1
 
 
 class TestAbstractTask(unittest.TestCase):
+    """
+    Contains unit tests for
+    :class:`mr_freeze.tasks.abstract_task.AbstractTask`
+    """
     executor = mock.MagicMock(spec=Executor)
 
     def setUp(self):
+        """
+        Instantiate a concrete task
+        """
         self.task = ConcreteTask()
 
 
 class TestCall(TestAbstractTask):
+    """
+    Tests that calling the task with an executor successfully submits the task
+    """
 
     def test_submit(self):
+        """
+        Tests that the callable task was successfully submitted to the
+        executor
+        """
         self.task(self.executor)
 
         self.assertTrue(self.executor.submit.called)
