591e2fda1c836e62e2bd278e9ff3f67f1d26db20
Michal Kononenko <michalkononenko@gmail.com>
2017-03-02 16:08:22 -0500

    Wrote a setup py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+Installs the application to the python directory
+"""
+from distutils.core import setup
+
+setup(
+    name="mr_freeze",
+    version="1.0",
+    description="Reports measured variables from a cryostat",
+    author="Michal Kononenko",
+    author_email="mkononen@uwaterloo.ca",
+    packages=["mr_freeze"],
+    install_requires=[
+        "instrumentkit==0.3.1",
+        "typing==3.5.3.0"
+    ]
+)
