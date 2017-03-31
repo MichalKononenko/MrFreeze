72920210019017db0df5043b10dd4399704bec5b
Michal Kononenko <michalkononenko@gmail.com>
2017-03-31 14:17:43 -0400

    Updated build script so it wgets PyQt.

---
+++ tests/unit/test_ui/test_ui_loader.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8
+"""
+Contains unit tests for :mod:`mr_freeze.ui.ui_loader
+"""
+import unittest
+
+
+class TestUIDefaultState(unittest.TestCase):
+    """
+    Tests that the UI loads correctly
+    """
